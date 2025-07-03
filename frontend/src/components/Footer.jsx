b7fb2e388a6f04f6d1c1c01004b25ccfa3bb21a3
Likhitha J <likhithajlikhi@gmail.com>
2025-07-03 14:44:58 +0530

    initial commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="text-center py-8">
       <p className="text-sm text-slate-500">
-        EEG Emotion Analysis ::Developed by Team
+        <b>EEG Emotion Analysis :: Developed by Likhitha J</b>
       </p>
     </footer>
   );
